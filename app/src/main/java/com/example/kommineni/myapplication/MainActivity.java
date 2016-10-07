dde4047b3e403690368f42101e68948d37b84aee
kommineni <veeru.kommineni99@gmail.com>
2016-10-07 17:01:12 -0400

    Added Log statements

--- app/src/main/java/com/example/kommineni/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kommineni/myapplication/MainActivity.java
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         System.out.print("Inside the FirstActivity in MyApplication");
+        System.out.print("Inside the FirstActivity in MyApplication");
+        System.out.print("Inside the FirstActivity in MyApplication");
+        System.out.print("Inside the FirstActivity in MyApplication");System.out.print("Inside the FirstActivity in MyApplication");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
