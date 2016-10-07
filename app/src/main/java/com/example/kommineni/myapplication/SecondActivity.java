9ab594ae45d0a5bd22ddf2a5d07a5fb78c720d0d
kommineni <veeru.kommineni99@gmail.com>
2016-10-07 16:28:09 -0400

    Added Activity and related xml file

---
+++ app/src/main/java/com/example/kommineni/myapplication/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.kommineni.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KOMMINENI on 10/7/2016.
+ */
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
