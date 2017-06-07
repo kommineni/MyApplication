a36aff7c6fe5c70e82facf53361c9de317225e96
Veeranarayana Kommineni <vkommineni@pinsightmedia.com>
2017-06-07 15:19:59 -0500

    no message

--- app/src/main/java/com/example/kommineni/myapplication/ActivityThree.java
+++ app/src/main/java/com/example/kommineni/myapplication/ActivityThree.java
@@ -9,6 +9,9 @@ public class ActivityThree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+<<<<<<< HEAD
         System.out.println("This is last screen \n Tesing for new feature");
+=======
+>>>>>>> parent of e8290fe... no message
     }
 }
